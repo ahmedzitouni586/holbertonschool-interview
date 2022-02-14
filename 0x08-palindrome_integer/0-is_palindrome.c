412cf5d03efcaf056af78f21263796d0e9055ae6
ahmedzitouni586 <zitouniahmed595@gmail.com>
2022-02-14 18:42:28 +0100

    done

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -15,3 +15,19 @@ unsigned long getbeg(unsigned long n)
 	while (n /= 10);
 	return (beg / 10);
 }
+
+/**
+ * is_palindrome - checks whether given unsigned integer is a palindrome
+ * @n: number to check
+ *
+ * Return: 1 if palindrome, otherwise 0
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long beg, end;
+
+	for (beg = getbeg(n), end = 1; beg > end; beg /= 10, end *= 10)
+		if (n / beg % 10 != n / end % 10)
+			return (0);
+	return (1);
+}
