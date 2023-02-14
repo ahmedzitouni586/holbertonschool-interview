9f18bc6bce5b7d0720e7f4cf30bc6619e3cd23d6
Ahmed <zitouniahmed595@gmail.com>
2023-02-14 15:20:23 +0100

    done

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
+
 def isWinner(x, nums):
-    def is_prime(num):
-        if num < 2:
+    def is_prime(n):
+        if n <= 1:
             return False
-        for i in range(2, int(num**(1/2)) + 1):
-            if num % i == 0:
+        for i in range(2, int(n**(0.5)) + 1):
+            if n % i == 0:
                 return False
         return True
 
     def game(n):
-        if n < 2:
+        if n <= 1:
             return "Ben"
-        for i in range(2, int(n**(1/2)) + 1):
-            if n % i == 0:
-                return "Maria" if game(i) == "Ben" else "Ben"
+        for i in range(2, n + 1):
+            if is_prime(i):
+                if game(n - i) == "Maria":
+                    return "Ben"
         return "Maria"
 
-    maria_count = 0
-    ben_count = 0
+    maria_wins = 0
+    ben_wins = 0
     for n in nums:
-        winner = game(n)
-        if winner == "Maria":
-            maria_count += 1
-        elif winner == "Ben":
-            ben_count += 1
-
-    if maria_count > ben_count:
-        return "Maria"
-    elif ben_count > maria_count:
+        if game(n) == "Maria":
+            maria_wins += 1
+        else:
+            ben_wins += 1
+    if ben_wins > maria_wins:
         return "Ben"
+    elif maria_wins > ben_wins:
+        return "Maria"
     else:
         return None
 
-
-isWinner = isWinner
-
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+print("Winner: {}".format(isWinner(3, [4, 5, 1])))
